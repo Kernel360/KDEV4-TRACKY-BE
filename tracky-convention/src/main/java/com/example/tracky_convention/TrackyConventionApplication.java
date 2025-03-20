d9012d9229a368b081624e819138b03d336096e8
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-20 12:15:07 +0900

    convention example

---
+++ tracky-convention/src/main/java/com/example/tracky_convention/TrackyConventionApplication.java
@@ -0,0 +1,13 @@
+package com.example.tracky_convention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackyConventionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackyConventionApplication.class, args);
+	}
+
+}
---
+++ tracky-convention/src/test/java/com/example/tracky_convention/TrackyConventionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tracky_convention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackyConventionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
