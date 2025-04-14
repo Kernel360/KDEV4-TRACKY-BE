2a0b4fdf6a76677e0d7e81eac7bf07051e0e9f42
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-04-14 20:23:34 +0900

    Update DriveHistoryApiDocs.java

--- tracky-web/src/main/java/kernel360/trackyweb/drivehistory/presentation/DriveHistoryApiDocs.java
+++ tracky-web/src/main/java/kernel360/trackyweb/drivehistory/presentation/DriveHistoryApiDocs.java
@@ -16,7 +16,7 @@ import kernel360.trackyweb.drivehistory.domain.RentDriveHistroyWithDriveList;
 public interface DriveHistoryApiDocs {
 
 	@Operation(summary = "예약자 조회", description = "예약자 관련 운행 정보 조회")
-	ApiResponse<List<RentDriveHistroyWithDriveList>> findAllRentHistories(
+	ApiResponse<List<RentDriveHistoryWithDriveList>> findAllRentHistories(
 		@RequestParam(required = false) String rentUuid
 	);
 
